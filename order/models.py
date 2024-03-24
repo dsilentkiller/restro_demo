08e77a5418fb2579e5d12e0407fd4b5613103850
dsilentkiller <paarurawal55@gmail.com>
2024-03-24 09:06:48 +0545

    create table Table Inventory

--- order/models.py
+++ order/models.py
@@ -9,4 +9,9 @@ class Table(models.Model):
     def __str__(self):
         return f"{self.table_name}={self.floor}"
     
-# class menu(models.Model)
+class menu(models.Model):
+    food_name=models.CharField(max_length=100)
+    price=models.FloatField()
+
+    def __str__(self):
+        return f"{self.food_name}={self.price}"
