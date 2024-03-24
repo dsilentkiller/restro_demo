a81a635aa42291ee229490aa0d4d888bf88877df
dsilentkiller <paarurawal55@gmail.com>
2024-03-24 09:03:47 +0545

    create table Table Inventory

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+CHOICES = (('kg', 'kg'),
+           ('gm', 'gm'),
+           ('piece', 'piece'),
+           ('ml', 'ml'),
+           ('liter', 'liter'),
+           ('plate', 'plate'),)
+
+
+
+
+class Inventory(models.Model):
+    ingredient_name = models.CharField(max_length=100, default='maida')
+    ingredient_quantity = models.FloatField()
+    ingredient_price = models.FloatField()
+    ingredient_unit = models.CharField(max_length=50, choices=CHOICES)
+
+    def __str__(self):
+        return f"{self.ingredient_name}={self.ingredient_quantity} {self.ingredient_unit},{self.ingredient_price}"
+    
+
--- order/models.py
+++ order/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Table(models.Model):
+    table_name = models.CharField(max_length=50)
+    floor = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.table_name}={self.floor}"
+    
+# class menu(models.Model)
